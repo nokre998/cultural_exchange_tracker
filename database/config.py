6707bc2bc4ab992b41a28e21b3e576c2f1f3dc6b
nokre998 <erkezhan090906@gmail.com>
2026-02-09 21:01:06 +0500

    Try to build architecture

---
+++ database/config.py
@@ -0,0 +1,45 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_SERVER: str = "smtp.mail.ru"
+    MAIL_PORT: int = 587
+    MAIL_USE_TLS: bool = True
+    gmail_user: str
+    gmail_app_password: str
+
+    URI: str | None = None
+    URL_DATABASE: str | None = None
+
+    def reload_env(self):
+        load_dotenv(override=True)
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DATABASE_URL_sync(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+    TOKEN_SECRET_KEY: str
+
+settings = Settings()
---
+++ database/db.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from database.config import settings
+from sqlalchemy.orm import DeclarativeBase
+import asyncio
+
+
+class Base(DeclarativeBase):
+    pass
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True
+)
+
+async_session = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False
+)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+async def async_main():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    asyncio.run(async_main())
+
+
